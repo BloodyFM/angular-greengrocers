b5e2b7600124240cb4b1fb448ddc340e7b477f62
Gudbrand <gudbrandwd@live.no>
2024-03-14 11:41:55 +0100

    sorting

--- src/app/groceries/list/list.component.ts
+++ src/app/groceries/list/list.component.ts
@@ -12,6 +12,7 @@ export class ListComponent {
 
   items: Item[] = [];
   filter: string = 'none';
+  sort: string = 'none';
 
   async ngOnInit() {
     this.items = await this.groceriesService.shopItems;
@@ -19,15 +20,31 @@ export class ListComponent {
     console.log(this.items);
   }
 
+  sortItems = () => {
+    if (this.sort === 'price') {
+      this.items.sort((a, b) => a.price - b.price);
+    }
+    if (this.sort === 'name') {
+      this.items.sort((a, b) => a.name.localeCompare(b.name));
+    }
+  };
+
   filterItems = () => {
     if (this.filter !== 'none') {
       this.items = this.items.filter((i) => i.type === this.filter);
     }
+    this.sortItems();
   };
 
   filterClicked = async (newFilter: string) => {
     this.items = await this.groceriesService.shopItems;
     this.filter = newFilter;
+    this.filterItems();
+  };
+
+  sortClicked = async (newSort: string) => {
+    this.items = await this.groceriesService.shopItems;
+    this.sort = newSort;
     this.filterItems();
   };
 
