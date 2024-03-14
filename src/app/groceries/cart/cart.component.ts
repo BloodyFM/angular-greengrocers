490ce6dc6c2cd1f09d46ad4277b47f1afcedef1c
Gudbrand <gudbrandwd@live.no>
2024-03-14 10:34:24 +0100

    cart done

--- src/app/groceries/cart/cart.component.ts
+++ src/app/groceries/cart/cart.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
+import { CartItem, Item } from 'src/app/models/item';
+import { GroceriesService } from '../services/groceries.service';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+  styleUrls: ['./cart.component.css'],
 })
 export class CartComponent {
+  constructor(private readonly groceriesService: GroceriesService) {}
 
+  items: CartItem[] = this.groceriesService.cartItems;
+
+  updateItems = (): void => {
+    this.items = this.groceriesService.cartItems;
+  };
+
+  cartItemToItem = (item: CartItem): Item => {
+    return {
+      id: item.id,
+      name: item.name,
+      price: item.price,
+    };
+  };
+
+  addToCart = (item: CartItem): void => {
+    this.groceriesService.addToCart(this.cartItemToItem(item));
+    this.updateItems();
+  };
+
+  removeFromCart = (item: CartItem): void => {
+    this.groceriesService.removeFromCart(this.cartItemToItem(item));
+    this.updateItems();
+  };
 }
